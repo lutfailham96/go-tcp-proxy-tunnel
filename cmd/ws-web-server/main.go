e7fbf121683c2e6e2aa18d0db64f72b5cd966d5f
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-10-14 16:31:04 +0700

    Hard refactor webserver main binary

--- cmd/ws-web-server/main.go
+++ cmd/ws-web-server/main.go
@@ -1,311 +1,60 @@
 package main
 
 import (
-	"bytes"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
-	"math/big"
+	proxy "github.com/lutfailham96/go-tcp-proxy-tunnel"
 	"net"
-	"net/http"
-	"strings"
 	"sync"
-	"time"
 )
 
 var (
 	httpAddress    = flag.String("l", "0.0.0.0:80", "http listen address")
 	httpsAddress   = flag.String("ln", "0.0.0.0:443", "https listen address")
 	backendAddress = flag.String("b", "127.0.0.1:8082", "backend proxy address")
-	domain         = flag.String("d", "*", "allowed server host address / domain, separated by comma: 'myserver.tls,anotherserver.tld'")
 )
-
-const (
-	cerTypeCA   = 0
-	cerTypeCert = 1
-)
-
-type serverConfig struct {
-	secure  bool
-	address string
-	cer     []tls.Certificate
-}
 
 func main() {
 	flag.Parse()
 
 	var tcpWg sync.WaitGroup
 
-	webConfig := &serverConfig{
-		secure:  false,
-		address: *httpAddress,
-	}
-
-	serverTLSConf, _, err := tlsCertSetup()
-	if err != nil {
-		fmt.Printf("Setup tls certificate error '%s'", err)
-		return
-	}
-	webTlsConfig := &serverConfig{
-		secure:  true,
-		address: *httpsAddress,
-		cer:     serverTLSConf.Certificates,
-	}
-
-	fmt.Printf("Websocket web server running on %s, %s\n\n", *httpAddress, *httpsAddress)
-
 	tcpWg.Add(2)
-	go startServer(&tcpWg, webConfig)
-	go startServer(&tcpWg, webTlsConfig)
+	go setupTcpListener(false)
+	go setupTcpListener(true)
 
 	tcpWg.Wait()
 }
 
-func startServer(wg *sync.WaitGroup, config *serverConfig) {
-	defer wg.Done()
-
-	mux := http.NewServeMux()
-
-	mux.Handle("/", http.HandlerFunc(mainHandler))
-
-	srv := &http.Server{
-		Addr:    config.address,
-		Handler: mux,
-	}
-
-	if config.secure {
-		srv.TLSConfig = &tls.Config{
-			Certificates:       config.cer,
-			InsecureSkipVerify: true,
-		}
-	}
-
+func setupTcpListener(secure bool) {
+	var ln net.Listener
 	var err error
-	if config.secure {
-		err = srv.ListenAndServeTLS("", "")
+
+	if secure {
+		tlsConfig, _, err := proxy.TlsCertSetup()
+		if err != nil {
+			fmt.Printf("Cannot setup tls certificates '%s'", err)
+		}
+		ln, err = tls.Listen("tcp", *httpsAddress, tlsConfig)
 	} else {
-		err = srv.ListenAndServe()
+		ln, err = net.Listen("tcp", *httpAddress)
 	}
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	if !isWebsocket(r) {
-		http.Error(w, "Not valid websocket request", http.StatusInternalServerError)
-		fmt.Printf("Not valid websocket request (%s >> %s)\n", r.RemoteAddr, *backendAddress)
+		fmt.Printf("Cannot bind port '%s'\n", err)
 		return
 	}
 
-	domainValid := false
-	if *domain != "*" {
-		for _, allowedDomain := range strings.Split(*domain, ",") {
-			if r.Host == allowedDomain {
-				domainValid = true
-				break
-			}
-		}
-	}
-	if !domainValid && *domain != "*" {
-		http.Error(w, "Domain not allowed", http.StatusForbidden)
-		fmt.Printf("Domain not allowed (%s >> %s)\n", r.RemoteAddr, *backendAddress)
-		return
-	}
-
-	fmt.Printf("Serving websocket proxy (%s >> %s)\n", r.RemoteAddr, *backendAddress)
-	p := websocketProxy(*backendAddress)
-	p.ServeHTTP(w, r)
-}
-
-func isWebsocket(r *http.Request) bool {
-	connUpgrade := r.Header.Get("Upgrade")
-
-	if strings.ToLower(connUpgrade) == "websocket" {
-		return true
-	}
-
-	return false
-}
-
-func websocketProxy(target string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dst, err := net.Dial("tcp", target)
-		if err != nil {
-			http.Error(w, "Error contacting backend server", http.StatusInternalServerError)
-			fmt.Printf("Error dialing websocket backend %s: %v", target, err)
-			return
-		}
-		defer closeConnection(dst)
-
-		src, err := createHijack(w)
-		if err != nil {
-			return
-		}
-		defer closeConnection(src)
-
-		err = r.Write(dst)
-		if err != nil {
-			fmt.Printf("Error copying request to target: %v", err)
-			return
-		}
-
-		errSrcCh := make(chan error, 1)
-		errDstCh := make(chan error, 1)
-		go forwardConn(src, dst, errDstCh)
-		go forwardConn(dst, src, errSrcCh)
-		<-errDstCh
-		<-errSrcCh
-	})
-}
-
-func forwardConn(src io.Reader, dst io.Writer, errCh chan error) {
-	buff := make([]byte, 0xffff)
+	connId := uint64(0)
 	for {
-		nr, err := src.Read(buff)
-		b := buff[0:nr]
+		src, err := ln.Accept()
 		if err != nil {
-			//fmt.Printf("Cannot read buff '%s'", err)
-			errCh <- err
-			return
+			fmt.Printf("Cannot accept connection '%s'\n", err)
+			continue
 		}
-		_, err = dst.Write(b)
-		if err != nil {
-			//fmt.Printf("Cannot write buff '%s'", err)
-			errCh <- err
-			return
-		}
+		connId += 1
+		fwd := proxy.NewWebForwarder(connId, src)
+		fwd.SetDstAddress(*backendAddress)
+		go fwd.Start()
 	}
-}
-
-func createHijack(w http.ResponseWriter) (net.Conn, error) {
-	hj, ok := w.(http.Hijacker)
-	if !ok {
-		http.Error(w, "can't switch protocols", http.StatusInternalServerError)
-		fmt.Printf("can't switch protocols using non-Hijacker ResponseWriter type %T", w)
-		return nil, errors.New(fmt.Sprintf("can't switch protocols using non-Hijacker ResponseWriter type %T", w))
-	}
-
-	nc, _, err := hj.Hijack()
-	if err != nil {
-		http.Error(w, "Hijack failed on protocol switch", http.StatusInternalServerError)
-		fmt.Printf("Hijack failed on protocol switch: %v", err)
-		return nil, errors.New(fmt.Sprintf("Hijack failed on protocol switch: %v", err))
-	}
-
-	return nc, nil
-}
-
-func closeConnection(conn net.Conn) {
-	err := conn.Close()
-	if err != nil {
-		fmt.Printf("Cannot close connection '%s'", err)
-		return
-	}
-}
-
-func tlsCertSetup() (serverTLSConf *tls.Config, clientTLSConf *tls.Config, err error) {
-	ca := generateX509Cer(cerTypeCA)
-
-	caPrivateKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivateKey.PublicKey, caPrivateKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	caPEM := new(bytes.Buffer)
-	pem.Encode(caPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: caBytes,
-	})
-
-	caPrivateKeyPEM := new(bytes.Buffer)
-	pem.Encode(caPrivateKeyPEM, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(caPrivateKey),
-	})
-
-	cert := generateX509Cer(cerTypeCert)
-
-	certPrivateKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivateKey.PublicKey, caPrivateKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certPEM := new(bytes.Buffer)
-	pem.Encode(certPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certBytes,
-	})
-
-	certPrivateKeyPEM := new(bytes.Buffer)
-	pem.Encode(certPrivateKeyPEM, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(certPrivateKey),
-	})
-
-	serverCert, err := tls.X509KeyPair(certPEM.Bytes(), certPrivateKeyPEM.Bytes())
-	if err != nil {
-		return nil, nil, err
-	}
-
-	serverTLSConf = &tls.Config{
-		Certificates: []tls.Certificate{serverCert},
-	}
-
-	certPool := x509.NewCertPool()
-	certPool.AppendCertsFromPEM(caPEM.Bytes())
-	clientTLSConf = &tls.Config{
-		RootCAs: certPool,
-	}
-
-	return
-}
-
-func generateX509Cer(cerType uint) *x509.Certificate {
-	cer := &x509.Certificate{
-		SerialNumber: big.NewInt(2022),
-		Subject: pkix.Name{
-			Organization:  []string{"WS"},
-			Country:       []string{"WS"},
-			Province:      []string{"WS"},
-			Locality:      []string{"WS"},
-			StreetAddress: []string{"WS"},
-			PostalCode:    []string{"00000"},
-		},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-	}
-
-	if cerType == cerTypeCA {
-		cer.IsCA = true
-		cer.KeyUsage = x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign
-		cer.BasicConstraintsValid = true
-	}
-
-	if cerType == cerTypeCert {
-		cer.KeyUsage = x509.KeyUsageDigitalSignature
-		cer.IPAddresses = []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback}
-	}
-
-	return cer
 }
